832ee2c3173979e0c512e96aab2407eac6567d69
linoyit <linoyhd@gmail.com>
2021-12-11 18:25:16 +0200

    responsive matches

--- src/components/Matches.tsx
+++ src/components/Matches.tsx
@@ -1,10 +1,12 @@
 import { useAppSelector } from "../store/hooks";
 import { User } from "../store/matchingSlice";
+import { useCollection } from "../utils/useCollection";
 import MatchBox from "./MatchBox";
 import './Matches.css';
 
 const Matches: React.FC = () => {
-    const matches = useAppSelector((state) => state.matching.matches);
+
+    const {matches} = useCollection();
     return (
         <div className="container">
             {matches.map((user: User) => <MatchBox user={user} key={user.username} />)}
